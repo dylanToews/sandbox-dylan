d511c1079170964a072cef645eb64636f0816a30
SheepHappens <110347843+dylanToews@users.noreply.github.com>
2023-08-30 14:41:36 -0600

    cleanup

--- assets/td-tabs.js
+++ assets/td-tabs.js
@@ -2,48 +2,37 @@
 if (!customElements.get("td-tabs-element")) {
   class tabCustomElement extends HTMLElement {
     constructor() {
-      super();
-      
+      super();     
       this.heading_selector = "[data-tab-heading]";
       this.content_selector = "[data-tab-content]";
       this.activeIndex = 0;
     }
-
     connectedCallback() {
       setTimeout(() => {
-
         this.attachListeners();
       });
     }
-
     attachListeners() {
-
       this.querySelectorAll(this.heading_selector).forEach((element, index) => {
         element.addEventListener("click", (event) => {
-
           this.setActiveTab(index);
         });
       });
     }
-
     setActiveContent(index) {
-      const contentElements = document.querySelectorAll(this.content_selector)
-  
+      const contentElements = document.querySelectorAll(this.content_selector)  
       if (contentElements[index].hasAttribute("hidden")) {
         contentElements[index].removeAttribute("hidden");
       } else {
         contentElements[index].setAttribute("hidden", "true");
       }
     }
-
     setActiveTab(index) {
       const headingElements = document.querySelectorAll(this.heading_selector);
-
       headingElements[this.activeIndex].setAttribute("aria-selected", "true");
       headingElements[index].setAttribute("aria-selected", "false");
       this.setActiveContent(index);
-      this.activeIndex = index;
-      
+      this.activeIndex = index;      
     }
   }
 
